fe93f02003ebc9680715de44e4327b5e4e025e98
Jorge Aparicio <japaricious@gmail.com>
2015-02-02 08:08:44 +0000

    docs!

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = {};
+searchIndex['parallel'] = {"items":[[0,"","parallel","(Hopefully) Safe fork-join parallelism abstractions"],[5,"divide","","Parallelizes an `operation` over a mutable slice"],[5,"fork","","Spawns an unsafe thread that may outlive its captured references."]],"paths":[]};
+searchIndex['parallel_macros'] = {"items":[[0,"","parallel_macros",""],[14,"execute!","","Executes several closures in parallel"]],"paths":[]};
+
+initSearch(searchIndex);
